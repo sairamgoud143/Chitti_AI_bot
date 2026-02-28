3e64586633bcd58317c31a54e8186644ea69d596
sairamgoud143 <sairamamaragonda4@gmail.com>
2026-02-28 21:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def verify_password(pswd):
             [],
             gr.update(visible=True),
             'Access Granted !',
-            gr.update(value='',visible=True),
+            gr.update(value=None,visible=True),
             gr.update(visible=True),
             gr.update(visible=True),
         )
@@ -24,7 +24,7 @@ def verify_password(pswd):
             [],
             gr.update(visible=False),
             'Invalid Credentials ! Please Login with correct credentials',
-            gr.update(value = '',visible=True),
+            gr.update(value =None,visible=True),
             gr.update(visible=False),
             gr.update(visible=False),
         )
@@ -221,6 +221,7 @@ with gr.Blocks(theme = gr.themes.Monochrome()) as demo:
 port = int(os.environ.get('PORT',7860))
 demo.launch(server_name = '0.0.0.0',
             server_port = port)
+
 
 
 
